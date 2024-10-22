e9e18fae5ef3045053d415c5a6665e581b0b1291
SweetLanaSison <sweetlanasison@gmail.com>
2024-10-23 00:20:02 +0800

    push ko muna

--- routes/jobroutes.js
+++ routes/jobroutes.js
@@ -463,6 +463,23 @@ router.get('/api/jobs/export', async (req, res) => {
         return res.status(500).json({ message: 'Server error while generating CSV.', error: e.message });
     }
 });   
+//kunin muna user id 
+// Get user by wallet address
+router.get('/users/wallet/:walletAddress', async (req, res) => {
+    const { walletAddress } = req.params;
+
+    try {
+        const user = await User.findOne({ walletAddress: walletAddress });
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.status(200).json({ userId: user._id });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
 
 //MARVIN ITO
 router.post('/transaction-notifications', async (req, res) => {
