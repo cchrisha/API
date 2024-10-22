7af793ce061bc7cab85e1a50bea26dfc20a9139c
mjmacam6 <mjmacam6@gmail.com>
2024-10-22 11:48:11 +0800

    zx

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(jobRoutes); 
 app.use(cors());
-app.use("/api",require(".routes/app.routes"));
+app.use('/api',require('.routes/app.routes'));
 
 // Configure Cloudinary with your credentials
 cloudinary.config({
