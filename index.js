5a0510913929133280f645332ed00051765169bf
mjmacam6 <mjmacam6@gmail.com>
2024-10-22 12:00:14 +0800

    aaa

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const verifyToken = require('./middleware/auth');
 const cloudinary = require('cloudinary').v2;
 const jobRoutes = require('./routes/jobroutes'); 
 const nodemailer = require('nodemailer');
-const appRoutes = require('./routes/routes.js');
+//const appRoutes = require('./routes/routes.js');
 const app = express();
 app.use(express.json());
 app.use(jobRoutes); 
 app.use(cors());
-app.use(appRoutes);
+//app.use(appRoutes);
 //app.use('/api', require('.routes/app.routes'));
 
 // Configure Cloudinary with your credentials
