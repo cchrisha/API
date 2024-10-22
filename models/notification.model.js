8f5c370a390073a4408b143a361c56b06686b690
mjmacam6 <mjmacam6@gmail.com>
2024-10-22 19:33:56 +0800

    ...

--- models/notification.model.js
+++ models/notification.model.js
@@ -8,16 +8,5 @@ const notificationSchema = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now } // Timestamp
 });
 
-const userTransactionSchema = new mongoose.Schema({
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // The user sending the transaction
-    receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // The user receiving the transaction
-    amount: { type: Number, required: true }, // Amount involved in the transaction
-    transactionDate: { type: Date, default: Date.now }, // Timestamp for the transaction
-    status: { type: String, enum: ['completed', 'failed'], default: 'completed' }, // Status of the transaction
-});
-
-const UserTransaction = mongoose.model('UserTransaction', userTransactionSchema);
 const Notification = mongoose.model('Notification', notificationSchema);
-
-module.exports = Notification, UserTransaction;
-
+module.exports = Notification;
