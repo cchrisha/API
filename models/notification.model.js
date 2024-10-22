4a6d94914b511342057e1355765bc97ff56a3d7e
mjmacam6 <mjmacam6@gmail.com>
2024-10-22 20:41:45 +0800

    zx

--- models/notification.model.js
+++ models/notification.model.js
@@ -12,11 +12,10 @@ const notificationSchema = new mongoose.Schema({
 const transactionNotificationSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // The user receiving the notification
     message: { type: String, required: true }, // Notification message
-    transaction: { type: mongoose.Schema.Types.ObjectId, ref: 'Transaction' }, // Related transaction
+    transactionHash: { type: String, required: true }, // Hash of the transaction
     isRead: { type: Boolean, default: false }, // To track if the notification has been read
     createdAt: { type: Date, default: Date.now } // Timestamp
 });
-
 
 const Notification = mongoose.model('Notification', notificationSchema);
 const TransactionNotification = mongoose.model('TransactionNotification', transactionNotificationSchema);
