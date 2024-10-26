6e00297cd7da68521e9d90593ef116eef7bb1639
chrisha <153200985+cchrisha@users.noreply.github.com>
2024-10-26 18:23:19 +0800

    eeck

--- models/verificationnotification.model.js
+++ models/verificationnotification.model.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose');
 const VerificationNotificationSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // The admin who receives the notification
     message: { type: String, required: true },
-    isRead: { type: Boolean, default: false }, // To track if the notification has been read.
+    isRead: { type: Boolean, default: false }, // Track if the notification has been read
     requestedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // The user who made the request
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now } // Auto-set to the current date
 });
 
 const VerificationNotification = mongoose.model('VerificationNotification', VerificationNotificationSchema);
 
 module.exports = { VerificationNotification };
+
 
 
 // const mongoose = require('mongoose');
